161107a0627328ad6954f6de3ec272cd5ae752af
Anton Mametov <joyfulanton1107@gmail.com>
2024-04-17 22:47:38 +0300

    fin

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,40 +5,40 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const delay = Number(form.nextElementSibling.delay.value);
-    const state = form.elements.state.value;
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
 
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (state === 'fulfilled') {
-                resolve(delay);
-            } else {
-                reject(delay);
-            }
-        }, delay);
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+  promise
+    .then(value => {
+      iziToast.show({
+        message: `✅ OK Fulfilled promise in ${value} ms`,
+        messageSize: '16',
+        messageLineHeight: '24',
+        messageColor: '#fff',
+        color: '#59a10d',
+        position: 'topRight',
+      });
+    })
+    .catch(error => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${error} ms`,
+        messageSize: '16',
+        messageLineHeight: '24',
+        messageColor: '#fff',
+        color: '#ef4040',
+        position: 'topRight',
+      });
     });
-    promise
-        .then(value => {
-            iziToast.show({
-                message: `✅ OK Fulfilled promise in ${value} ms`,
-                messageSize: '16',
-                messageLineHeight: '24',
-                messageColor: '#fff',
-                color: '#59a10d',
-                position: 'topRight',
-            });
-        })
-        .catch(error => {
-            iziToast.show({
-                message: `❌ Rejected promise in ${error} ms`,
-                messageSize: '16',
-                messageLineHeight: '24',
-                messageColor: '#fff',
-                color: '#ef4040',
-                position: 'topRight',
-            });
-        });
-    form.reset();
+  form.reset();
 }
