148cd9a24a13db18c7fea055f100e60f7e90fbd5
Anton Mametov <joyfulanton1107@gmail.com>
2024-04-18 19:12:05 +0300

    fin

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,17 +1,16 @@
-import flatpickr from 'flatpickr';
+ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const ref = {
-  
-    currentDays: document.querySelector('span[data-days]'),
-    currentHours: document.querySelector('span[data-hours]'),
-    currentMinutes: document.querySelector('span[data-minutes]'),
-    currentSeconds: document.querySelector('span[data-seconds]'),
-    btnStart: document.querySelector('button[data-start]'),
-    dateTimePicker: document.querySelector('#datetime-picker'),
+  currentDays: document.querySelector('span[data-days]'),
+  currentHours: document.querySelector('span[data-hours]'),
+  currentMinutes: document.querySelector('span[data-minutes]'),
+  currentSeconds: document.querySelector('span[data-seconds]'),
+  btnStart: document.querySelector('button[data-start]'),
+  dateTimePicker: document.querySelector('#datetime-picker'),
 };
 
 let userSelectedDate = null;
@@ -21,7 +20,7 @@ let diffDate = null;
 ref.btnStart.setAttribute('disabled', 'disabled');
 
 ref.dateTimePicker.addEventListener('click', () => {
-    ref.btnStart.removeAttribute('disabled', 'disabled');
+  ref.btnStart.removeAttribute('disabled', 'disabled');
 });
 
 const options = {
@@ -84,5 +83,5 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    return { days, hours, minutes, seconds };
+  return { days, hours, minutes, seconds };
 }
